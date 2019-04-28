1848c4bd63a6c4eccba2b113299c006ecea74d05
proegssilb <minifig404@gmail.com>
2019-04-28 17:25:20 -0500

    Minor tweaks.

--- config-builder/main.sh
+++ config-builder/main.sh
@@ -3,15 +3,15 @@
 # Config line syntax: inFile:outFile[:owner:group[:mode]]
 
 while read CONFIGLINE; do
-  INFILE=$(echo CONFIGLINE | cut -d ':' -f 1)
-  OUTFILE=$(echo CONFIGLINE | cut -d ':' -f 2)
-  OWNER=$(echo CONFIGLINE | cut -d ':' -f 3)
-  GROUP=$(echo CONFIGLINE | cut -d ':' -f 4)
-  MODE=$(echo CONFIGLINE | cut -d ':' -f 5)
+  INFILE=$(echo $CONFIGLINE | cut -d ':' -f 1)
+  OUTFILE=$(echo $CONFIGLINE | cut -d ':' -f 2)
+  OWNER=$(echo $CONFIGLINE | cut -d ':' -f 3)
+  GROUP=$(echo $CONFIGLINE | cut -d ':' -f 4)
+  MODE=$(echo $CONFIGLINE | cut -d ':' -f 5)
   envsubst <${INFILE} >${OUTFILE}
-  if [ -n "$OWNER" -a -n "$GROUP"]; then
+  if [ -n "$OWNER" -a -n "$GROUP" ] ; then
     chown $OWNER:$GROUP $OUTFILE
-    if [ -n "$MODE" ]; then
+    if [ -n "$MODE" ] ; then
       chmod $MODE $OUTFILE
     fi
   fi
